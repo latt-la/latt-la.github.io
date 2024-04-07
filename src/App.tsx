f7e93fdf5f6e9221930c57fe477749b33bc206a9
Virkkunen <vrkknn@vrkknn.net>
2024-04-07 14:59:25 +0200

    add logo svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import Block from './components/Block';
 import Footer from './components/Footer';
+import LogoFull from './components/logo/LogoFull';
 
 function App() {
   return (
     <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48'>
       <Block>
         <div className='flex flex-row justify-between items-center'>
-          <div className='font-medium text-2xl'>
-            <span>Latt-la</span>
-          </div>
+          <LogoFull />
         </div>
       </Block>
       <Footer />
