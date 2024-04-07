89bead732309470a3cc06e01883226372987bce5
Virkkunen <vrkknn@vrkknn.net>
2024-04-07 13:58:57 +0200

    create block component

---
+++ src/components/Block.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+
+interface Block {
+  children: ReactNode;
+  title?: string;
+  buttonGroup?: boolean;
+}
+
+const Block: React.FC<Block> = ({ children, title, buttonGroup }) => {
+  return (
+    <div
+      className={`flex flex-col justify-between p-6 bg-slate-600 rounded-2xl xl:col-start-4 xl:col-span-6 shadow-md`}
+    >
+      {title && <span className='font-bold text-2xl mb-6'>{title}</span>}
+      {buttonGroup ? (
+        <div className='flex max-lg:flex-col max-lg:justify-between flex-wrap gap-x-2 gap-y-4 xl:gap-4'>
+          {children}
+        </div>
+      ) : (
+        <>{children}</>
+      )}
+    </div>
+  );
+};
+
+export default Block;
