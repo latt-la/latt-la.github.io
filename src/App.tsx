b763f755815d3868b34fbfa65163938c8b6f5ba7
Virkkunen <vrkknn@vrkknn.net>
2024-04-07 15:01:00 +0200

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import Block from './components/Block';
 import Footer from './components/Footer';
 import LogoFull from './components/logo/LogoFull';
 
 function App() {
   return (
     <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48'>
-      <Block>
-        <div className='flex flex-row justify-between items-center'>
-          <LogoFull />
-        </div>
-      </Block>
+      <div className='flex flex-col justify-between p-6 xl:col-start-4 xl:col-span-6'>
+        <LogoFull />
+      </div>
       <Footer />
     </div>
   );
