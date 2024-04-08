de6894c05bf80f9ca9e89d73d3fcf256ac97e43c
Virkkunen <vrkknn@vrkknn.net>
2024-04-08 11:18:13 +0200

    adding a fg colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       fontFamily: { 'roboto': ['Roboto', 'sans'] },
       colors: {
         'latt-bg': '#f4deba',
+        'latt-fg': '#fff0c2',
         'latt-purple': '#542141',
       }
     },
