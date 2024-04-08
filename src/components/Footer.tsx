c6570db6ac4140bca100827dab406eece35f54a1
Virkkunen <vrkknn@vrkknn.net>
2024-04-08 09:55:24 +0200

    mobile layout fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-between p-6 xl:col-start-4 xl:col-span-6'>
-      <div className='flex flex-row justify-around xl:mx-auto xl:gap-16'>
+    <div className='flex flex-col justify-between p-4 xl:p-6 xl:col-start-4 xl:col-span-6'>
+      <div className='flex flex-col xl:flex-row justify-center text-center xl:justify-around mx-auto xl:gap-16 gap-y-2 xl:gap-y-0'>
         <div>
           <span>© 2024 Latt-la</span>
         </div>
 
-        <span className=''>•</span>
+        <span className='max-lg:hidden'>•</span>
 
-        <div className='flex flex-row gap-2'>
+        <div className='flex flex-row gap-2 justify-center'>
           <Link
             to='/terms'
             className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
@@ -21,9 +21,9 @@ const Footer = () => {
           </Link>
         </div>
 
-        <span className=''>•</span>
+        <span className='max-lg:hidden'>•</span>
 
-        <div className='flex flex-row gap-2'>
+        <div className='flex flex-row gap-2 justify-center'>
           <span>Made by</span>
           <Link
             className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
