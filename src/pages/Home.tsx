b81ccef8ff2ed1606cd408a61d4830ab756e1feb
Virkkunen <vrkknn@vrkknn.net>
2024-04-10 10:34:12 +0200

    testing a new layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
+import Block from '../components/Block';
 import LogoFull from '../components/logo/LogoFull';
 
 function App() {
   return (
-    <div className='flex flex-col justify-between p-6 xl:col-start-4 xl:col-span-6'>
-      <LogoFull />
-    </div>
+    <>
+      <div className='flex flex-col justify-between p-6 xl:col-start-2 xl:col-span-4 xl:row-start-1'>
+        <LogoFull />
+      </div>
+      <div className='flex flex-col justify-center p-6 xl:col-start-7 xl:col-span-4 xl:row-start-1'>
+        <Block>
+          <div>test</div>
+        </Block>
+      </div>
+    </>
   );
 }
 
