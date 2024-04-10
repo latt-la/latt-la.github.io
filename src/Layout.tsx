87a4e4ea96591a23bd6407080313bbd2143c93f4
Virkkunen <vrkknn@vrkknn.net>
2024-04-10 10:44:59 +0200

    layout changes

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,8 +3,10 @@ import Footer from './components/Footer';
 
 const Layout = () => {
   return (
-    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 xl:h-full xl:place-content-center xl:my-auto xl:min-h-screen gap-y-8 xl:py-48'>
+    <div className='flex flex-col xl:grid xl:grid-cols-12 xl:row-auto max-md:p-6 h-screen place-content-center my-auto min-h-screen gap-y-8 xl:py-48'>
+      <div className='max-lg:my-auto lg:hidden'></div>
       <Outlet />
+      <div className='max-lg:mb-auto lg:hidden'></div>
       <Footer />
     </div>
   );
