c1564d37103b7393a6f9b6e4f4cefa45c0fa56d5
Virkkunen <vrkknn@vrkknn.net>
2024-04-10 10:33:31 +0200

    hopefully fix wraping on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,63 +3,46 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-between p-4 xl:p-6 xl:col-start-4 xl:col-span-6'>
-      <div className='flex flex-col xl:flex-row justify-center text-center xl:justify-around mx-auto xl:gap-16 gap-y-2 xl:gap-y-0'>
-        <div>
-          <span className='flex flex-row gap-x-[0.35rem] justify-center align-middle'>
-            <Link
-              to='/'
-              className='font-medium hover:text-blue-500 active:text-blue-300 transition-all ease-in-out duration-200'
-            >
-              Lattla.art
-            </Link>{' '}
-            © 2024 by{' '}
-            <Link
-              className='flex flex-row justify-center items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
-              target='_blank'
-              rel='noreferrer noopener'
-              to='https://vrkknn.net'
-            >
-              <FaCat />
-              <span>Virkkunen</span>
-            </Link>{' '}
-            is licensed under{' '}
-            <Link
-              to='http://creativecommons.org/licenses/by-nc-nd/4.0/'
-              className='flex flex-row justify-center items-center gap-x-1 font-medium transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
-            >
-              CC BY-NC-ND 4.0
-              <FaCreativeCommons />
-              <FaCreativeCommonsBy />
-            </Link>
-          </span>
-        </div>
-
-        <span className='max-lg:hidden'>•</span>
-
-        <div className='flex flex-row gap-2 justify-center'>
+    <div className='flex flex-col flex-wrap max-lg:text-sm xl:flex-row justify-center items-center text-center xl:justify-around xl:gap-16 gap-y-2 xl:gap-y-1 xl:p-6 xl:col-start-4 xl:col-span-6'>
+      <div className='flex flex-row flex-wrap justify-center gap-y-1 gap-x-1 lg:gap-x-1 lg:gap-y-0 items-center'>
           <Link
-            to='/terms'
-            className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+            to='/'
+            className='font-medium hover:text-blue-500 active:text-blue-300 transition-all ease-in-out duration-200'
           >
-            <FaFileInvoice />
-            <span>Terms of Service</span>
-          </Link>
-        </div>
+            Lattla.art
+          </Link>{' '}
+          © 2024 by
 
-        {/* <div className='flex flex-row gap-2 justify-center'>
-          <span>Made by</span>
-          <Link
-            className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
-            target='_blank'
-            rel='noreferrer noopener'
-            to='https://github.com/Virkkunen'
-          >
-            <FaGithub />
-            <span>Virkkunen</span>
-          </Link>
-        </div> */}
+        <Link
+          className='flex flex-row justify-center items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+          target='_blank'
+          rel='noreferrer noopener'
+          to='https://vrkknn.net'
+        >
+          <FaCat />
+          <span>Virkkunen</span>
+        </Link>
+
+        <span>is licensed under</span>
+        <Link
+          to='http://creativecommons.org/licenses/by-nc-nd/4.0/'
+          className='flex flex-row justify-center items-center gap-x-1 font-medium transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+        >
+          CC BY-NC-ND 4.0
+          <FaCreativeCommons />
+          <FaCreativeCommonsBy />
+        </Link>
       </div>
+
+      <span className='max-2xl:hidden'>•</span>
+
+      <Link
+        to='/terms'
+        className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+      >
+        <FaFileInvoice />
+        <span>Terms of Service</span>
+      </Link>
     </div>
   );
 };
