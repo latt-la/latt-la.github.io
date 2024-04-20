d5cda87ad28b24258b08f53ae66c1cc5b3dab4e4
Virkkunen <vrkknn@vrkknn.net>
2024-04-20 12:41:03 +0200

    wee update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,35 @@
-import { FaCat, FaCreativeCommons, FaCreativeCommonsBy, FaFileInvoice } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
+import {
+  FaCat,
+  FaCreativeCommons,
+  FaCreativeCommonsBy,
+  FaFileInvoice,
+} from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col flex-wrap max-lg:text-sm xl:flex-row justify-center items-center text-center xl:justify-around xl:gap-16 gap-y-2 xl:gap-y-1 xl:p-6 xl:col-start-4 xl:col-span-6 max-lg:flex-row-reverse'>
-      <div className='flex flex-row flex-wrap justify-center gap-y-1 gap-x-1 lg:gap-x-1 lg:gap-y-0 items-center'>
-          <Link
-            to='/'
-            className='font-medium hover:text-blue-500 active:text-blue-300 transition-all ease-in-out duration-200'
-          >
-            Lattla.art
-          </Link>{' '}
-          © 2024 by
-
+    <div className="flex flex-col flex-wrap max-lg:text-sm xl:flex-row justify-center items-center text-center xl:justify-around xl:gap-16 gap-y-2 xl:gap-y-1 xl:p-6 xl:col-start-4 xl:col-span-6 max-lg:flex-row-reverse">
+      <div className="flex flex-row flex-wrap justify-center gap-y-1 gap-x-1 lg:gap-x-1 lg:gap-y-0 items-center">
         <Link
-          className='flex flex-row justify-center items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
-          target='_blank'
-          rel='noreferrer noopener'
-          to='https://vrkknn.net'
+          to="/"
+          className="font-medium hover:text-blue-500 active:text-blue-300 transition-all ease-in-out duration-200"
+        >
+          Lattla.art
+        </Link>{" "}
+        © 2024 by
+        <Link
+          className="flex flex-row justify-center items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600"
+          target="_blank"
+          rel="noreferrer noopener"
+          to="https://vrkknn.net"
         >
           <FaCat />
           <span>Virkkunen</span>
         </Link>
-
-        <span>is licensed under</span>
+        |
         <Link
-          to='http://creativecommons.org/licenses/by-nc-nd/4.0/'
-          className='flex flex-row justify-center items-center gap-x-1 font-medium transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+          to="http://creativecommons.org/licenses/by-nc-nd/4.0/"
+          className="flex flex-row justify-center items-center gap-x-1 font-medium transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600"
         >
           CC BY-NC-ND 4.0
           <FaCreativeCommons />
@@ -34,11 +37,11 @@ const Footer = () => {
         </Link>
       </div>
 
-      <span className='max-2xl:hidden'>•</span>
+      <span className="max-2xl:hidden">•</span>
 
       <Link
-        to='/terms'
-        className='flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600'
+        to="/terms"
+        className="flex flex-row items-center gap-x-1 transition-all ease-in-out duration-200 hover:text-blue-400 active:text-blue-600"
       >
         <FaFileInvoice />
         <span>Terms of Service</span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,10 @@
-import Block from '../components/Block';
-import Button from '../components/Button';
-import LogoFull from '../components/logo/LogoFull';
+import LogoFull from "../components/logo/LogoFull";
 
 function App() {
   return (
     <>
-      <div className='flex flex-col justify-between p-6 xl:col-start-2 xl:col-span-4 xl:row-start-1'>
+      <div className="flex flex-col justify-between p-6 xl:col-start-4 xl:col-span-6 xl:row-start-1">
         <LogoFull />
-      </div>
-      <div className='flex flex-col justify-center p-6 xl:col-start-7 xl:col-span-4 xl:row-start-1 gap-y-2'>
-        <Block>
-          <div className='flex flex-col lg:flex-row justify-center items-center gap-2'>
-            test
-            <Button
-              text='first'
-              link='/'
-            />
-            <Button
-              text='second'
-              link='/'
-            />
-            <Button
-              text='third'
-              link='/'
-            />
-            <Button
-              text='fourth'
-              link='/'
-            />
-          </div>
-        </Block>
       </div>
     </>
   );
